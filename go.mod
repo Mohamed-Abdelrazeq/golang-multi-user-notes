8cfc2f150827e95a81b0160e830f05a7195e1eb8
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-08-17 20:28:06 +0300

    jwt scatch

--- go.mod
+++ go.mod
@@ -3,17 +3,23 @@ module github.com/Fiber-CRUD
 go 1.20
 
 require (
-	github.com/google/uuid v1.3.0
+	github.com/gofiber/contrib/jwt v1.0.4
 	github.com/gofiber/fiber/v2 v2.48.0
-	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0
+	github.com/google/uuid v1.3.0
 	github.com/lib/pq v1.10.9
+)
+
+require (
+	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.48.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,34 +2,51 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"time"
 
 	handler "github.com/Fiber-CRUD/controllers"
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/golang-jwt/jwt/v5"
 	_ "github.com/lib/pq"
 )
 
 func main() {
 	app := fiber.New()
-	app.Use(logger.New())
 
+	// MIDDLEWARES
+	app.Use(logger.New())
+	app.Use("/api", jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte("secret")},
+	}))
+	// DB INIT
 	dataSource, err := openDB()
 	if err != nil {
 		log.Fatal("ERROR CONNECTING TO DB")
 	}
 
+	app.Route("/authenticate", func(router fiber.Router) {
+		router.Post("/login", login)
+		router.Get("/", accessible)
+		router.Get("/restricted", restricted)
+	})
+
 	app.Route("/api", func(router fiber.Router) {
+		// NOTES
 		router.Get("/notes/:id", dataSource.GetNoteById)
 		router.Delete("/notes/:id", dataSource.DeleteNote)
 		router.Post("/notes", dataSource.CreateNote)
 		router.Patch("/notes", dataSource.UpdateNote)
 		router.Get("/notes", dataSource.GetAllNotes)
+		// HEALTH
 		router.Get("/health", handler.CheckHealth)
 	})
-	log.Fatal(
-		app.Listen("127.0.0.1:8080"),
-	)
+
+	// START SERVER
+	log.Fatal(app.Listen("127.0.0.1:8080"))
 }
 
 func openDB() (*handler.DataSource, error) {
@@ -42,4 +59,49 @@ func openDB() (*handler.DataSource, error) {
 	}
 
 	return &handler.DataSource{DB: conn}, nil
+}
+
+func login(c *fiber.Ctx) error {
+	println("LOGIN")
+	user := c.FormValue("user")
+	pass := c.FormValue("pass")
+
+	fmt.Println("1")
+	// Throws Unauthorized error
+	if user != "john" || pass != "doe" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	fmt.Println("2")
+	// Create the Claims
+	claims := jwt.MapClaims{
+		"name":  "John Doe",
+		"admin": true,
+		"exp":   time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	fmt.Println("3")
+	// Create token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token": t})
+}
+
+func accessible(c *fiber.Ctx) error {
+	fmt.Println("ACC")
+	return c.SendString("Accessible")
+}
+
+func restricted(c *fiber.Ctx) error {
+	fmt.Println("RES")
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.SendString("Welcome " + name)
 }

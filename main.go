c56a048fe4202aafcb34cd4902b4bc21a47b86a8
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-08-17 20:43:50 +0300

    jwt parser

--- main.go
+++ main.go
@@ -73,3 +73,10 @@ func login(c *fiber.Ctx) error {
 
 	return c.JSON(fiber.Map{"token": t})
 }
+
+// func jwtParser(c *fiber.Ctx) error {
+// 	user := c.Locals("user").(*jwt.Token)
+// 	claims := user.Claims.(jwt.MapClaims)
+// 	name := claims["name"].(string)
+// 	return c.SendString("Welcome " + name)
+// }
